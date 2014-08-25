40d565f257d05c04596e47649629ffbd7fd5b924
Pete Morici <pete.morici@alumni.purdue.edu>
2014-08-25 03:15:07 -0400

    switch list to deque

--- fibd.py
+++ fibd.py
@@ -1,10 +1,11 @@
 from sys import stdin
+from collections import deque
 
 def rabbit_mpop(n, m):
     """return rabbits alive after n months if they live for m months.
          Fn = Fn-1 + Fn-2 - Fn-m-1
     """
-    h = [0] * (m-2) + [1,1,1]
+    h = deque([0] * (m-2) + [1,1,1])
 
     if n < 2:
         return 1
@@ -13,7 +14,7 @@ def rabbit_mpop(n, m):
 
     for g in xrange(2, n):
         h.append(h[-1] + h[-2] - h[-(m+1)])
-        h.pop(0)
+        h.popleft()
 
     return h[-1]
         
